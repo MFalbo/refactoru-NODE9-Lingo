612bff72130a13da0e39482eaec7769d7b224c4b
Matthew Falbo <matthew.falbo@gmail.com>
2014-05-14 15:07:30 -0600

    functional translate page, form submit, renders a result page with translation and ability to submit a new translation

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var beGlobal = require('node-beglobal');
+var translate = require('./models/translate.js')
 
 var app = express();
 app.set('view engine', 'jade');
@@ -7,9 +9,31 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
+var beglobal = new beGlobal.BeglobalAPI({
+  api_token: 'QFQv%2ByR2w0YF9RyjqHagrw%3D%3D'
+});
+
 app.get('/', function(req, res) {
 	res.render('index');
 });
+
+
+app.get('/translate', function(req, res) {
+	res.render('translate');
+});
+
+app.post('/translate/result', function(req,res){
+	beglobal.translations.translate(
+	 	 {text: req.body.text, from: req.body.from, to: req.body.to},
+	  	function(err, results) {
+		    if (err) {
+		      return console.log(err);
+		    }
+			res.render('result', {results: results});
+		}
+	);	
+});
+
 
 var server = app.listen(8627, function() {
 	console.log('Express server listening on port ' + server.address().port);
---
+++ public/javascripts/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+// $.post('/translate', function(data) {
+// 	console.log(data);
+// 	beglobal.translations.translate(
+// 	 	 {text: data.text, from: data.from, to: data.to},
+// 	  	function(err, results) {
+// 	    if (err) {
+// 	      return console.log(err);
+// 	    }
+// 	    // translation = results.translation;
+// 	    // console.log(results);
+// 	    // res.render('translate', {translation: results});
+// 	    $('body').append('<p>' + results.translation)
+// 	});
+// });
+
+});
